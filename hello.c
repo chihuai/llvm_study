66ac80be8c3c771735e07449d3b9637051ab8e09
chihuai <s505063@gmail.com>
2015-02-03 12:51:33 +0800

    hahahaha2

--- hello.c
+++ hello.c
@@ -6,5 +6,20 @@ int main(){
     printf("hello world\n");
     printf("hello world\n");
     printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
+    printf("hello world\n");
     return 0;
 }
